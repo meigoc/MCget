4bc3ea1449becf37930e5ea392689494c19ca9e6
Meigo <73817505+meigoc@users.noreply.github.com>
2023-12-30 23:08:21 +0300

    🔥 Update v1.9.2 | Download NOW 🔥
    🔥 Update v1.9.2 | Download NOW 🔥

--- start.bat
+++ start.bat
@@ -1,2 +1,8 @@
-java -Xmx1024M -jar MCget-nogui.jar
+@echo off
+chcp 65001
+echo V1.9 Downloaded from https://github.com/meigoc/MCget/
+echo --------------------
+java -Xmx1024M -jar "McGet.v2.0.jar"
+echo --------------------
+echo Downloaded from https://github.com/meigoc/MCget/
 pause

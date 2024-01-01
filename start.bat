273208ea1cc2823d7f2e2012b7943f754eafe54b
Meigo <73817505+meigoc@users.noreply.github.com>
2024-01-02 01:16:22 +0300

    🔥 Update v1.9.5 | Download NOW 🔥
    🔥 Update v1.9.5 | Download NOW 🔥

--- start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001
-echo V1.9 Downloaded from https://github.com/meigoc/MCget/
-echo --------------------
-java -Xmx1024M -jar "McGet.v2.0.jar"
-echo --------------------
-echo Downloaded from https://github.com/meigoc/MCget/
-pause
